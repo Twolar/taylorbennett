2eaa429c7ac2fab7c008c8833bb59edbc4b650c6
“Taylor <t4ybennett@gmail.com>
2025-01-20 13:13:39 +0000

    Convert to component for blog posts + modify fetch post methods

--- app/blog/[category]/[postId]/page.js
+++ app/blog/[category]/[postId]/page.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { getPostData, getSortedPostsData } from "@/lib/blogPosts";
+import Link from "next/link";
 
 const BlogPostPage = ({ params }) => {
   const { category, postId } = params;
   const postData = getPostData(category, postId);
 
-  // TODO: Add category buttons, with current highlighted and have hover highlighting
-
-  // TODO: Refactor repitition into components...
-
   return (
     <div className="min-h-screen flex flex-col">
       <h1 className="text-3xl font-bold text-primary">{postData.title}</h1>
       <p className="mt-2 italic text-sm">{postData.date}</p>
-      <p className="mt-2 italic text-sm">{postData.category}</p>
+      <div className="flex flex-wrap gap-4 mt-3">
+        <Link
+          key={category}
+          href={`/blog/${category}`}
+          className="btn-sm bg-accent rounded px-3 py-1"
+        >
+          {category}
+        </Link>
+      </div>
       <div className="divider divider-accent"></div>
       {postData.description && (
         <>
@@ -32,12 +37,12 @@ const BlogPostPage = ({ params }) => {
 
 export default BlogPostPage;
 
-// Generate paths for all posts
+// Generate static paths for all posts
 export async function generateStaticParams() {
   const allPostsData = getSortedPostsData();
 
   return allPostsData.map((post) => ({
-    category: post.category,
-    postId: post.id,
+    category: post.category.toLowerCase(),
+    postId: post.id.toString(),
   }));
 }
--- app/blog/[category]/page.js
+++ app/blog/[category]/page.js
@@ -1,48 +1,26 @@
 import React from "react";
 import { getSortedPostsData } from "@/lib/blogPosts";
-import Link from "next/link";
+import BlogPosts from "@/app/(components)/BlogPosts";
 
 const BlogCategoryPage = ({ params }) => {
   const { category } = params;
-  const allPostsData = getSortedPostsData();
-
-  // TODO: Highlight hover for categories
-
-  // Filter posts by category
-  const filteredPosts = allPostsData.filter(
-    (post) => post.category === category
-  );
+  const allPostsData = getSortedPostsData(category);
 
   return (
     <div className="min-h-screen flex flex-col">
-      <h1 className="text-5xl font-bold text-primary">Blog</h1>
-      <div className="flex flex-wrap gap-4 mt-6">
-        <p className="btn-sm bg-accent rounded px-3 py-1">{category}</p>
-      </div>
-      <div className="divider divider-accent"></div>
-      {filteredPosts.map(({ id, date, title }) => (
-        <div key={id}>
-          <h2 className="text-l font-bold text-primary">
-            <Link href={`/blog/${category}/${id}`}>{title}</Link>
-          </h2>
-          <p className="text-sm">
-            <span className="italic">{date}</span>
-          </p>
-          <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
-        </div>
-      ))}
+      <BlogPosts categories={[category]} posts={allPostsData} />
     </div>
   );
 };
 
 export default BlogCategoryPage;
 
-// Generate paths for categories at build time
+// Generate static paths for all categories
 export async function generateStaticParams() {
   const allPostsData = getSortedPostsData();
   const categories = [...new Set(allPostsData.map((post) => post.category))];
 
   return categories.map((category) => ({
-    category,
+    category: category.toLowerCase(),
   }));
 }
